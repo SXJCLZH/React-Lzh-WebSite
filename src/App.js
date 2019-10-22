0ea222544d5ff78330dde20213c89c02189fdf71
lzh <lizhenhui0808@aliyun.com>
2019-10-22 19:01:19 +0800

    准备回家改

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'antd';
-
+import { Card  } from 'antd';
+const { Meta } = Card;
 function App() {
   return (
-    <div className="App">
-     <Button type="primary">Button</Button> 
-    </div>
+    <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
   );
 }
 
